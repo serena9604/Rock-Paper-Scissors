e90100337a3ba42d2fc562c25250a560058fc3b9
Serena Bi <56049445+serena9604@users.noreply.github.com>
2019-10-18 08:26:26 -0700

    Add files via upload

--- Rock Paper Scissors/RPS1.java
+++ Rock Paper Scissors/RPS1.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class RPS
+public class RPS1
 {
     //prints out which choice each player made
     public static void choice (String playerChoice, String computerChoice) {
@@ -26,7 +26,8 @@ public class RPS
             System.out.println("Good luck human...");
             System.out.println("");
 
-            while (turn != ROCK && turn != PAPER && turn != SCISSORS) { //loops until player makes valid choice
+            //while (turn != ROCK && turn != PAPER && turn != SCISSORS) { //loops until player makes valid choice
+            while (turn != ROCK) {
                 System.out.println("What do you throw?  [1] Rock, [2] Paper, [3] Scissors?"); //player turn
                 turn = sc.nextInt();
 
@@ -35,8 +36,8 @@ public class RPS
                 } 
 
             } 
-            
-            computerTurn = (int) Math.random()*3; //computer makes turn
+
+            computerTurn = (int) (Math.random()*3) + 1; //computer makes turn
             System.out.println(computerTurn);
 
         }
---
+++ Rock Paper Scissors/__SHELL7.java
@@ -0,0 +1,8 @@
+
+public class __SHELL7 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+RPS1.main(__bluej_param0);
+
+}}
